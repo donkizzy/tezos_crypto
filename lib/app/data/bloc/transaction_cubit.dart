28c29812135e59c3e504fff8888e3d98d17c47d8
Amos Godwin <amosgodwin500@gmail.com>
2024-11-28 19:36:53 +0100

    added cubit to for transactions and transactions blocks

---
+++ lib/app/data/bloc/transaction_cubit.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tezos_asessment/app/data/models/transaction_block_response.dart';
+import 'package:tezos_asessment/app/data/models/transaction_response.dart';
+import 'package:tezos_asessment/app/data/repositories/transaction_repository.dart';
+
+part 'transaction_state.dart';
+
+class TransactionCubit extends Cubit<TransactionState> {
+  late TransactionRepository _transactionRepository;
+
+  TransactionCubit([ TransactionRepository? transactionRepository]) : super(TransactionInitial()){
+    _transactionRepository = transactionRepository ?? TransactionRepository(dio: Dio());
+  }
+
+  void fetchLatestBlock() async {
+    emit(TransactionBlockLoading());
+    try {
+      var response = await _transactionRepository.fetchLatestBlock();
+      emit(TransactionBlockSuccess(transactionBlockResponse: response));
+    } catch (e) {
+      emit(TransactionBlockError(error: e.toString()));
+    }
+  }
+
+  void fetchTransactions({required String hashBlock}) async {
+    emit(TransactionLoading());
+    try {
+      var response = await _transactionRepository.fetchTransactions(hashBlock: hashBlock);
+      emit(TransactionSuccess(transactionResponse: response));
+    } catch (e) {
+      emit(TransactionError(error: e.toString()));
+    }
+  }
+}
---
+++ lib/app/data/bloc/transaction_state.dart
@@ -0,0 +1,53 @@
+part of 'transaction_cubit.dart';
+
+sealed class TransactionState extends Equatable {
+  const TransactionState();
+}
+
+final class TransactionInitial extends TransactionState {
+  @override
+  List<Object> get props => [];
+}
+
+final class TransactionBlockLoading extends TransactionState {
+  @override
+  List<Object> get props => [];
+}
+
+final class TransactionBlockSuccess extends TransactionState {
+  final TransactionBlockResponse transactionBlockResponse;
+
+  const TransactionBlockSuccess({required this.transactionBlockResponse});
+  @override
+  List<Object> get props => [transactionBlockResponse];
+}
+
+final class TransactionBlockError extends TransactionState {
+  final String error ;
+
+  const TransactionBlockError({required this.error});
+  @override
+  List<Object> get props => [error];
+}
+
+
+final class TransactionLoading extends TransactionState {
+  @override
+  List<Object> get props => [];
+}
+
+final class TransactionSuccess extends TransactionState {
+  final TransactionResponse transactionResponse;
+
+  const TransactionSuccess({required this.transactionResponse});
+  @override
+  List<Object> get props => [transactionResponse];
+}
+
+final class TransactionError extends TransactionState {
+  final String error ;
+
+  const TransactionError({required this.error});
+  @override
+  List<Object> get props => [error];
+}
